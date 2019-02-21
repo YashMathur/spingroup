d185cfe670ca2e7b16b25bf4e8d54fe447e116f5
Yash Mathur <yash.mathur@grandrounds.com>
2019-02-20 20:14:56 -0800

    make Spingroup spin in parallel

--- spingroup.go
+++ spingroup.go
@@ -1,18 +1,29 @@
 package spingroup
 
 import (
+	"fmt"
 	"sync"
+	"time"
 
 	"./pkg/tasks"
+	"./pkg/ui"
 )
 
 // Spingroup struct holds information on the spin group
 type Spingroup struct {
 	chars []string
+	sleep time.Duration
 	tasks []*tasks.Task
 }
 
 var wg sync.WaitGroup
+
+// Create creates a Spingroup
+func Create(sleep time.Duration) *Spingroup {
+	return &Spingroup{
+		sleep: sleep,
+	}
+}
 
 // Add appends a task to the Spingroup
 func (sg *Spingroup) Add(name string, cmd ...string) {
@@ -25,21 +36,43 @@ func (sg *Spingroup) Add(name string, cmd ...string) {
 
 // Wait waits for all tasks in the spingroup to complete
 func (sg *Spingroup) Wait() {
+	var gui = ui.Create(len(sg.tasks))
+
+	for i, task := range sg.tasks {
+		row := gui.StartRow() + i
+
+		fmt.Print(ui.Move(row, 1))
+		fmt.Printf("  %s %d", task.Name(), i)
+	}
+
+	fmt.Print(ui.Hide())
+	iter := 0
+
 	for loop := true; loop; {
 		var allDone = true
 
-		for _, task := range sg.tasks {
-			if !task.IsDone() {
+		for i, task := range sg.tasks {
+			row := gui.StartRow() + i
+
+			fmt.Print(ui.Move(row, 1))
+
+			if !task.Done {
+				fmt.Print(gui.Spinner(iter))
 				allDone = false
 			} else {
-				allDone = true
+				fmt.Print("g")
 			}
 		}
 
 		if allDone {
 			break
 		}
+
+		iter = (iter + 1) % gui.Length()
+		time.Sleep(sg.sleep)
 	}
+	fmt.Printf("\n")
+	fmt.Print(ui.Show())
 
 	wg.Wait()
 }
